c86e17f21df05b229d13a250c6f0a7a2ec95b4da
Murad <mailprostoimail@gmail.com>
2022-06-27 23:00:40 +0300

    Task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result = ''
+  for (let i = String(n).length - 1; i >= 0; i--) {
+    if(String(n)[i] == '-') continue;
+    result += String(n)[i];
+  }
+
+  return result;
 }
